485add855083f5f7195ecb0225165adb659051ce
Manuel <manuel.t.shade@gmail.com>
2025-04-09 23:47:13 +0100

    Add application form to details component with submission handling

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -2,17 +2,25 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HousingLocation } from '../housing-location';
 import { HousingService } from '../housing.service';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
+  // router: Router = inject(Router)
   route: ActivatedRoute = inject(ActivatedRoute)
   housingService: HousingService = inject(HousingService)
   housingLocation: HousingLocation | undefined
+
+  applyForm = new FormGroup({
+    firstName: new FormControl(""),
+    lastName: new FormControl(""),
+    email: new FormControl("")
+  })
 
   constructor() {
 
@@ -24,5 +32,19 @@ export class DetailsComponent {
     // Alternatively, you can use the snapshot method to get the ID directly. Snapshot is a one-time read of the route parameters. Instantánea (sincrónica).
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId)
+  }
+
+  submitApplication() {
+    // Get the values from the form controls
+    const firstName = this.applyForm.get('firstName')?.value ?? "";
+    const lastName = this.applyForm.get('lastName')?.value ?? "";
+    const email = this.applyForm.get('email')?.value ?? "";
+
+    // Call the submitApplication method from the housing service
+    this.housingService.submitApplication(firstName, lastName, email);
+
+    // Redirect to the home page after submission
+    // this.router.navigate(['/home']);
+    console.log("Application submitted")
   }
 }
