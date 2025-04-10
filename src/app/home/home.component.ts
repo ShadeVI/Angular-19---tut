b544ed058accd003d5528aa41f412ce43f4e1c05
Manuel <manuel.t.shade@gmail.com>
2025-04-10 01:02:13 +0100

    Refactor home component to enhance form handling and filtering functionality

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,8 +12,24 @@ import { HousingLocation } from '../housing-location';
 export class HomeComponent {
   housingLocationList: HousingLocation[] = []
   housingService = inject(HousingService)
+  filteredLocationList: HousingLocation[] = []
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations()
+    this.housingService.getAllHousingLocations().then((locations) => {
+      this.housingLocationList = locations
+      this.filteredLocationList = locations
+    })
   }
+
+  filterResults(text: string) {
+    if (!text) this.filteredLocationList = this.housingLocationList
+    this.filteredLocationList = this.housingLocationList.filter(location => location.city.toLowerCase().includes(text.toLowerCase()))
+  }
+
+  resetHousingLocationList() {
+    this.filteredLocationList = this.housingLocationList
+    // We cannot clean the input field here because we are not using two-way data binding. We are using one-way data binding. The input field is not bound to a property in the component. In this case, we can use the reset attribute to clean the input.
+
+  }
+
 }
